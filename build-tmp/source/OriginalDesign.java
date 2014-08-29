2f1af08cdc08f589236deb6c43369d928820291f
unknown <maibarra@697-334-Gimli.sf.k12.internal>
2014-08-29 09:55:15 -0700

    updated with a complete design

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,15 +14,69 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
+int touchX = 300;
+int touchY = 300;
 public void setup()
 {
+	size(600,600);
+	background(222, 52, 40);
+	noStroke();
+	rect(0, 0, 100, 600);
+	rect(200, 0, 100, 600);
+	rect(400, 0, 100, 600);
 
 }
 public void draw()
 {
-  ellipse(50,50,50,50);
+ 	fill(0);
+ 	textSize(30);
+	text("CLICK & DRAG TO MAKE FUNNEL CAKE", 17, 65);
+	textSize(20);
+	text("press any key to clean up", 177, 95);
 }
 
+public void mouseDragged() 
+{
+	fill(230,194,112);
+	stroke(217, 182, 102);
+ 	ellipse(touchX,touchY,150,150);
+ 	touch();
+	redraw();
+}
+
+public void touch()
+{
+	touchX = pmouseX;
+	touchY = pmouseY;
+}
+public void mouseReleased() 
+{
+	sprinkle();
+}
+
+public void sprinkle() {
+	int numOfSprinkles = 0;
+	while(numOfSprinkles < 150) 
+	{
+		int sprinkleX = ((int)(random(0, 600)));
+		int sprinkleY = ((int)(random(0, 600)));
+		fill(255);
+		ellipse(sprinkleX, sprinkleY, 6, 6);
+		numOfSprinkles ++;
+
+	}
+}
+
+public void keyPressed()
+{
+	noStroke();
+	fill(222, 52, 40);
+	rect(0, 0, 600, 600);
+	fill(255);
+	rect(0, 0, 100, 600);
+	rect(200, 0, 100, 600);
+	rect(400, 0, 100, 600);
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
